6ce81441e37f600ddd4b104f2e3113bb25dd8abd
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-03 11:15:47 +0200

    automated quick commit

--- install/fln.sh
+++ install/fln.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/bash
 
+# TODO exa, exa-wrapper, tmux
+
 NVIM_DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/stable/nvim.appimage"
 LF_DOWNLOAD_URL="https://github.com/gokcehan/lf/releases/download/r29/lf-linux-amd64.tar.gz"
 PURE_REPO_URL="https://github.com/sindresorhus/pure.git"
 ALACRITTY_THEMES_REPO_URL="https://github.com/alacritty/alacritty-theme"
+NON_GUI_PROGRAMS="zsh tmux vim"
+
+fln() {
+  ln -f --nolink "${1}" "${2}"
+}
 
 main() {
-  alias fln="ln -f --no-unlink"
+  if [[ ${1} == "apt" ]]; then
+    apt update && apt upgrade
+    apt install -y "${NON_GUI_PROGRAMS}"
+  elif [[ ${1} == "pacman" ]]; then
+    pacman -Syu
+    pacman -S --confirm "${NON_GUI_PROGRAMS}"
+  fi
 
-  # nvim + configs for vim and vscodevim   
-  if ! command -v nvim &> /dev/null; then
-    wget -O nvim ${NVIM_DOWNLOAD_URL} 
+  # nvim + configs for vim and vscodevim
+  if ! command -v nvim &>/dev/null; then
+    wget -O nvim ${NVIM_DOWNLOAD_URL}
     mv nvim /usr/bin
     chmod u+x /usr/bin/nvim
   fi
@@ -25,7 +38,7 @@ main() {
   fln vim/vscodevimrc ~/.vscodevimrc
 
   # lf
-  if ! command -v lf &> /dev/null; then
+  if ! command -v lf &>/dev/null; then
     wget ${LF_DOWNLOAD_URL}
     tar -xvzf lf*.tar.gz
     rm -f lf*.tar.gz
@@ -35,24 +48,24 @@ main() {
   fln lf/lfrc ~/.config/lf
 
   # alacritty
-  command -v cargo &> /dev/null || curl https://sh.rustup.rs -sSf | sh
-  cargo install alacritty 
+  if ! command -v alacritty &>/dev/null; then
+    command -v cargo &>/dev/null || curl https://sh.rustup.rs -sSf | sh
+    cargo install alacritty
+  fi
   mkdir -p ~/.config/alacritty/themes
   git clone ${ALACRITTY_THEMES_REPO_URL} ~/.config/alacritty/themes
-
   fln alacritty/alacritty.yml ~/.config/alacritty
+
+  # tmux
   fln tmux/tmux.conf ~/.tmux.conf
 
-  mkdir -p ~/.config/lf
-  fln lf/lfrc ~/.config/lf
-
+  # zsh
   fln zsh/zshrc ~/.zshrc
   mkdir -p ~/.config/zsh
   fln zsh/aliasrc ~/.config
   fln zsh/shortcutrc ~/.config
-  git clone ${PURE_REPO_URL} ~/.config/zsh/pure 2> /dev/null
+  git clone ${PURE_REPO_URL} ~/.config/zsh/pure 2>/dev/null
 
-  unalias fln
 }
 
 main "$@"
