5d6b99e28c036875611364f9e2c61be7c46c4c1a
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-30 13:56:10 +0200

    automated quick commit

--- install/cli-essentials.sh
+++ install/cli-essentials.sh
@@ -7,7 +7,6 @@ NVIM_DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/stable/nvi
 LF_DOWNLOAD_URL="https://github.com/gokcehan/lf/releases/download/r29/lf-linux-amd64.tar.gz"
 PURE_REPO_URL="https://github.com/sindresorhus/pure.git"
 NON_GUI_PROGRAMS=("zsh" "tmux" "vim.gtk3" "curl" "git")
-EXA_WRAPPER_URL="https://gist.githubusercontent.com/tachoknight/b57da58a3ddb4f3f52f8480a90757a9f/raw/45146476aa8adbcc128c5145fc15247882d7a742/exa-wrapper.sh"
 
 install() {
   if ! command -v "${1}" &>/dev/null; then
@@ -20,7 +19,7 @@ install_libfuse() {
   if ! dpkg -s libfuse2 &>/dev/null; then
     echo "Installing libfuse2..."
     sudo add-apt-repository -y universe &>/dev/null
-    install libfuse2
+    install libfuse2 > /dev/null
   fi
 }
 
@@ -50,22 +49,16 @@ main() {
   mkdir -p ~/.config/zsh
   git clone ${PURE_REPO_URL} ~/.config/zsh/pure &>/dev/null
 
-
-  # exa
-  curl ${EXA_WRAPPER_URL} > /tmp/exa-wrapper.sh
-  sudo chown "$(whoami)" /tmp/exa-wrapper.sh
-  sudo chmod u+x /tmp/exa-wrapper.sh
-  sudo mv /tmp/exa-wrapper.sh /usr/bin
-
- if ! command -v exa &>/dev/null; then
+ # bat and cargo
+ if ! command -v bat &>/dev/null; then
     # install cargo if needed
     if ! command -v cargo &>/dev/null; then
       curl https://sh.rustup.rs -sSf | sh
       # shellcheck source=/dev/null
       source "$HOME/.cargo/env"
     fi
-    echo "Installing exa..."
-    cargo install exa >/dev/null
+    echo "Installing bat..."
+    cargo install bat >/dev/null
   fi
-} 
+}
 main "$@"
