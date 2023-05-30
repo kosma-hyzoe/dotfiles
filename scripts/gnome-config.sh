98c21601dca7f0b2914f374afdb4c8b164ada95d
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-30 17:21:59 +0200

    automated quick commit

--- scripts/gnome-config.sh
+++ scripts/gnome-config.sh
@@ -4,10 +4,4 @@
 gsettings set org.gnome.desktop.peripherals.keyboard delay 200 # originally 300
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 15 # originally 20
 
-# set alacritty as default
-if command -v alacritty &>/dev/null; then
-  sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator "$(which alacritty)"
-  sudo update-alternatives --config x-terminal-emulator
-fi
-
 chsh -s "$(which zsh)" "$(whoami)"
