1d97274330c8db3c766ae91da1f8a96441e6a1e4
kosma <kosma@localhost.localdomain>
2023-09-25 22:59:30 +0200

    automated quick commit

--- scripts/gnome-config.sh
+++ scripts/gnome-config.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # faster hjkl-ing around
-gsettings set org.gnome.desktop.peripherals.keyboard delay 200 # originally 300
+gsettings set org.gnome.desktop.peripherals.keyboard delay 150 # originally 300
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 15 # originally 20
 
 
-# command -v gnome-control-center &> /dev/null || sudo apt install gnome-control-center
