d85b18c181e950d93ec19085103e164189d7d94b
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2023-11-19 18:51:44 +0100

    automated quick commit

--- scripts/gnome-config.sh
+++ scripts/gnome-config.sh
@@ -5,7 +5,9 @@ gsettings set org.gnome.desktop.peripherals.keyboard delay 150 # originally 300
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 15 # originally 20
 
 # set alacritty as default
-sudo update-alternatives --install /usr/bin/x-terminal-emulator \
-    x-terminal-emulator $(which alacritty) 50
-sudo update-alternatives --config x-terminal-emulator
 
+if command -v alacritty &> /dev/null; then
+    sudo update-alternatives --install /usr/bin/x-terminal-emulator \
+        x-terminal-emulator $(which alacritty) 50
+    sudo update-alternatives --config x-terminal-emulator
+fi
