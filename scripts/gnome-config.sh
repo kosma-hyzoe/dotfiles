3951267f7627ea35380853354a6ec1093d67eab0
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2023-12-20 10:34:07 +0100

    keyboard delay in gnome changed

--- scripts/gnome-config.sh
+++ scripts/gnome-config.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # faster hjkl-ing around
-gsettings set org.gnome.desktop.peripherals.keyboard delay 150 # originally 300
-gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 15 # originally 20
+gsettings set org.gnome.desktop.peripherals.keyboard delay 200 # originally 300
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 20 # originally 20
 
 # set alacritty as default
-
 if command -v alacritty &> /dev/null; then
     sudo update-alternatives --install /usr/bin/x-terminal-emulator \
         x-terminal-emulator $(which alacritty) 50
