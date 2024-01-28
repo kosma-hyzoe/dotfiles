c8ba7a93533652091bfdabe536718d220275883e
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2024-01-28 19:12:33 +0100

    group directories first in exa and lsd

--- scripts/gnome-config.sh
+++ scripts/gnome-config.sh
@@ -3,10 +3,3 @@
 # faster hjkl-ing around
 gsettings set org.gnome.desktop.peripherals.keyboard delay 200 # originally 300
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 20 # originally 20
-
-# set alacritty as default
-if command -v alacritty &> /dev/null; then
-    sudo update-alternatives --install /usr/bin/x-terminal-emulator \
-        x-terminal-emulator $(which alacritty) 50
-    sudo update-alternatives --config x-terminal-emulator
-fi
