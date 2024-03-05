759667b847509074c4ca997634a9d5a4523d5111
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2024-03-05 08:43:10 +0100

    automated quick commit

--- scripts/rc-link.sh
+++ scripts/rc-link.sh
@@ -2,43 +2,39 @@
 
 NESTED_TMUX=
 
-main() {
-  cd $(dirname "$0")/..
-  mkdir -p ~/.config
+cd $(dirname "$0")/..
+mkdir  -p ~/.config
 
-  # vim
-  mkdir ~/.vim
-  ln -f vim/vimrc ~/.vim
-  ln -f vim/colorscheme.vim ~/.vim
-  ln -f vim/remaps.vim ~/.vim
-  ln -f vim/plugins.vim ~/.vim
+# vim
+mkdir -p ~/.vim
+ln -f vim/vimrc ~/.vim
+ln -f vim/colorscheme.vim ~/.vim
+ln -f vim/remaps.vim ~/.vim
+ln -f vim/plugins.vim ~/.vim
 
-  # lf
-  mkdir ~/.config/lf
-  ln -f lfrc ~/.config/lf
+# lf
+mkdir -p ~/.config/lf
+ln -f lfrc ~/.config/lf
 
 
-  # tmux
-  [[ $NESTED_TMUX -eq 1 ]] && \
-      ln -f tmux/nested-tmux.conf ~/.tmux.conf || \
-      ln -f tmux/tmux.conf ~/.tmux.conf
+# tmux
+[[ $NESTED_TMUX -eq 1 ]] && \
+  ln -f tmux/nested-tmux.conf ~/.tmux.conf || \
+  ln -f tmux/tmux.conf ~/.tmux.conf
 
-  # shell
-  ln -f shell/zshrc ~/.zshrc
-  ln -f shell/bashrc ~/.bashrc
-  ln -f shell/aliasrc ~/.config
-  ln -f shell/lessrc ~/.config
+# shell
+ln -f shell/zshrc ~/.zshrc
+ln -f shell/bashrc ~/.bashrc
+ln -f shell/aliasrc ~/.config
+ln -f shell/lessrc ~/.config
 
-  # python
-  mkdir -p ~/.ipython/profile_default
-  ln -f python/ipython_config.py ~/.ipython/profile_default
-  ln -f python/flake8 ~/.config
+# python
+mkdir -p ~/.ipython/profile_default
+ln -f python/ipython_config.py ~/.ipython/profile_default
+ln -f python/flake8 ~/.config
 
-  # kitty
-  mkdir ~/.config/kitty
-  ln -f kitty.conf ~/.config/kitty
+# kitty
+mkdir -p ~/.config/kitty
+ln -f kitty.conf ~/.config/kitty
 
-  cd - > /dev/null
-}
-
-main "$@"
+cd - > /dev/null
