2d44a1268cb5d5584acd92c6ef8fa44ee582a8f3
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2024-03-09 20:59:54 +0100

    automated quick commit

--- scripts/rc-link.sh
+++ scripts/rc-link.sh
@@ -16,9 +16,7 @@ mkdir -p ~/.config/lf
 ln -f lf/lfrc ~/.config/lf
 ln -f lf/lf_kitty_preview ~/.local/scripts
 ln -f lf/lf_kitty_clean ~/.local/scripts
-ln -f lf/lfcd ~/.local/scripts
 ln -f lf/_lfip ~/.local/scripts
-ln -f lf/lfip ~/.local/scripts
 
 # tmux
 [[ $NESTED_TMUX -eq 1 ]] && \
