aa1691d9d3bb1a2b3c6c7d9ef4383f30c11aa636
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2024-03-12 15:34:10 +0100

    automated quick commit

--- scripts/rc-link.sh
+++ scripts/rc-link.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-cd $(dirname "$0")/..
-mkdir -p ~/.local/scripts
-mkdir  -p ~/.config
+cd "$(dirname "$0")/.." || exit
+mkdir -p "$HOME/.local/scripts"
+mkdir  -p "$HOME/.config"
 
 # vim
-mkdir -p ~/.vim
-ln -f vim/vimrc ~/.vim
-ln -f vim/colorscheme.vim ~/.vim
-ln -f vim/remaps.vim ~/.vim
-ln -f vim/plugins.vim ~/.vim
+mkdir -p "$HOME/.vim"
+ln -f vim/vimrc "$HOME/.vim"
+ln -f vim/colorscheme.vim "$HOME/.vim"
+ln -f vim/remaps.vim "$HOME/.vim"
+ln -f vim/plugins.vim "$HOME/.vim"
 
 # lf
-mkdir -p ~/.config/lf
-ln -f lf/lfrc ~/.config/lf
-ln -f lf/lf_kitty_preview ~/.local/scripts
-ln -f lf/lf_kitty_clean ~/.local/scripts
-ln -f lf/_lfip ~/.local/scripts
+mkdir -p "$HOME/.config/lf"
+ln -f lf/lfrc "$HOME/.config/lf"
+ln -f lf/lf_kitty_preview "$HOME/.local/scripts"
+ln -f lf/lf_kitty_clean "$HOME/.local/scripts"
+ln -f lf/_lfip "$HOME/.local/scripts"
 
 # tmux
-[[ $NESTED_TMUX -eq 1 ]] && \
-    ln -f tmux/nested-tmux.conf ~/.tmux.conf || \
-    ln -f tmux/tmux.conf ~/.tmux.conf
+[[ "$NESTED_TMUX" -eq 1 ]] && \
+    ln -f tmux/nested-tmux.conf "$HOME/.tmux.conf" || \
+    ln -f tmux/tmux.conf "$HOME/.tmux.conf"
 
 # less
-ln -f config/lessrc ~/.config
+ln -f config/lessrc "$HOME/.config"
 
 # shell
-ln -f shell/zshrc ~/.zshrc
-ln -f shell/bashrc ~/.bashrc
-ln -f shell/aliasrc ~/.config
+ln -f shell/zshrc "$HOME/.zshrc"
+ln -f shell/bashrc "$HOME/.bashrc"
+ln -f shell/aliasrc "$HOME/.config"
 
 # python
-mkdir -p ~/.ipython/profile_default
-ln -f python/ipython_config.py ~/.ipython/profile_default
-ln -f python/flake8 ~/.config
+mkdir -p "$HOME/.ipython/profile_default"
+ln -f python/ipython_config.py "$HOME/.ipython/profile_default"
+ln -f python/flake8 "$HOME/.config"
 
 # kitty
-mkdir -p ~/.config/kitty
-ln -f config/kitty.conf ~/.config/kitty
+mkdir -p "$HOME/.config/kitty"
+ln -f config/kitty.conf "$HOME/.config/kitty"
 
-cd - > /dev/null
+cd - > /dev/null || exit
