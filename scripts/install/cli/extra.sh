b7aecf087d9791700de5ecfeff1cc71d66c38045
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2024-08-02 00:38:08 +0200

    automated quick commit

--- scripts/install/cli/extra.sh
+++ scripts/install/cli/extra.sh
@@ -3,25 +3,23 @@
 source "$(dirname "${0}")/../header"
 
 DEPENDENCIES="build-essential"
-CARGO_PROGRAMS="bat exa zoxide ripgrep fd-find fcp"
-PIPX_PROGRAMS="pynvim"
+CARGO_PROGRAMS="bat exa zoxide ripgrep fd-find fcp" \
+    "tealdeer tree-sitter-cli"
+APT_PROGRAMS="python3-pynvim"
 
 ill $DEPENDENCIES
 
-## cargo
+# cargo
 if ! command -v cargo &>/dev/null; then
     curl https://sh.rustup.rs -sSf | sh
-    source "$HOME/.cargo/env"
 fi
+
+# apt
+ill $APT_PROGRAMS
 
 EXT_PKGM="cargo install --locked"
 source "$(dirname "${0}")/../header"
 ill "$CARGO_PROGRAMS"
-
-# pip/pipx
-command -v pipx &>/dev/null || \
-    python3 -m pip install --user pipx \
-    pipx ensurepath > /dev/null
 
 EXT_PKGM="pipx install --user --upgrade"
 source "$(dirname "${0}")/../header"
