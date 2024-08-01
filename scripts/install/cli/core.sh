9c855496aa55c5b84a85e9c779c811df316ac896
Kosma H <kosma.hyzoe@gmail.com>
2024-08-02 00:47:35 +0200

    automated quick commit

--- scripts/install/cli/core.sh
+++ scripts/install/cli/core.sh
@@ -7,8 +7,7 @@ source "$(dirname "$0")/../header"
 
 CAN_SUDO=1
 
-APT="zsh git curl wget vim xclip tmux htop shellcheck python3 progress" \
-    "trash-cli speedtest-cli"
+APT="zsh git curl wget vim xclip tmux htop shellcheck python3 progress trash-cli speedtest-cli"
 PURE_REPO_URL="https://github.com/kosma-hyzoe/pure.git"
 LF_DOWNLOAD_URL="https://github.com/gokcehan/lf/releases/download/r32/lf-linux-${ARCH}.tar.gz"
 
--- scripts/install/cli/extra.sh
+++ scripts/install/cli/extra.sh
@@ -39,9 +39,11 @@ fi
 
 # gh
 if ! command -v gh &>/dev/null && command -v apt &>/dev/null; then
-    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-        && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-        && sudo apt update
-    ill gh
+    (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+    && sudo mkdir -p -m 755 /etc/apt/keyrings \
+    && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+    && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
 fi
