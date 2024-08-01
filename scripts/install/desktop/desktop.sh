33332855caf7229724f4020f0da27323855b70b8
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2024-08-02 00:46:44 +0200

    automated quick commit

--- scripts/install/desktop/desktop.sh
+++ scripts/install/desktop/desktop.sh
@@ -1,4 +1,11 @@
 FLATPAK="tidal keepassxc qbittorrent libreoffice stretchly"
-APT_PROGRAMS="timeshift exfatprogs chromium-browser vlc kitty libavcodec-extra flatpak  gnome-software-plugin-flatpak syncthing neofetch"
-PIPX_PROGRAMS="pynvim"
+APT_PROGRAMS="timeshift exfatprogs chromium-browser vlc kitty libavcodec-extra flatpak gnome-software-plugin-flatpak neofetch python3-pynvim"
+
+# papirus
+wget -qO- https://git.io/papirus-icon-theme-install | sh
+cp usr/share/applications/syncthing-start.desktop ~/.config/autostart
+
+# nerdfonts
+curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
+getnf -i NerdFontsSymbolsOnly
 
