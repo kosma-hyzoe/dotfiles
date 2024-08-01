7b769ab9eb35708b016bfce6b02dcc2ee965c610
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2024-08-02 00:38:03 +0200

    automated quick commit

---
+++ scripts/install/desktop/desktop.sh
@@ -0,0 +1,4 @@
+FLATPAK="tidal keepassxc qbittorrent libreoffice stretchly"
+APT_PROGRAMS="timeshift exfatprogs chromium-browser vlc kitty libavcodec-extra flatpak  gnome-software-plugin-flatpak syncthing neofetch"
+PIPX_PROGRAMS="pynvim"
+
