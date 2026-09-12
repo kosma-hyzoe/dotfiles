c8e3d01a61601f42c29133a02505c23cfbe50928
M.K. Hyzorek <kosma.hyzoe@gmail.com>
2026-09-12 11:19:21 +0200

    automated quick commit

--- scripts/pdf_merge.py
+++ scripts/pdf_merge.py
@@ -1,9 +1,10 @@
 import glob
 import re
+import sys
 from PyPDF2 import PdfMerger
 
 m = PdfMerger()
 key = lambda s:[int(t) if t.isdigit() else t for t in re.split(r'(\d+)', s)]
 [m.append(f) for f in sorted(glob.glob('*.pdf'), key=key)]
-m.write('$out')
+m.write(sys.argv[1])
 
